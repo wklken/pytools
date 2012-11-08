25b4e8b1baebd6c77ab0da4e975b5a845cad68c4
wklken <wklken@yeah.net>
2012-11-08 16:02:49 +0800

    add logical comment for dataformat.py

--- data_process/dataformat.py
+++ data_process/dataformat.py
@@ -11,13 +11,14 @@
 #2012-06-22 add function to support multi output separators
 #2012-07-11 fix bug  line 44,add if
 #2012-09-03 rebuild functions,add help msg! version0.5
+#2012-11-08 last version edited by lingyue.wkl
+#           this py: https://github.com/wklken/pytools/blob/master/data_process/dataformat.py
 
 import os
 import sys
 import getopt
 import time
 import re
-
 
 #read file and get each line without \n
 def read_file(path):
@@ -29,6 +30,7 @@ def read_file(path):
 #处理一行，转为目标格式，返回目标行
 def one_line_proc(parts, total, ft_map, outsp, empty_fill, fill_with_sno):
     outline = []
+    #step1.获取每一列的值
     for i in range(1, total + 1):
         if i in ft_map:
             fill_index = ft_map[i]
@@ -39,11 +41,14 @@ def one_line_proc(parts, total, ft_map, outsp, empty_fill, fill_with_sno):
             else:
                 outline.append(handler_specal_part(parts[int(fill_index) - 1]))
         else:
+            #-s 选项生效，填充列号
             if fill_with_sno:
                 outline.append(str(i))
+            #否则，填充默认填充值
             else:
                 outline.append(empty_fill)
 
+    #step2.组装加入输出分隔符，支持多分隔符
     default_outsp = outsp.get(0,"\t")
     result = []
     outsize = len(outline)
@@ -51,15 +56,18 @@ def one_line_proc(parts, total, ft_map, outsp, empty_fill, fill_with_sno):
         result.append(outline[i])
         if i < outsize - 1:
             result.append(outsp.get(i + 1, default_outsp))
+    #step3.拼成一行返回
     return ''.join(result)
 
 #处理入口，读文件，循环处理每一行，写出
 #输入数据分隔符默认\t,输出数据默认分隔符\t
-def process(inpath, total, to, outpath, insp="\t", outsp="\t", empty_fill="", fill_with_sno=False, error_line_out=False):
+def process(inpath, total, to, outpath, insp, outsp, empty_fill, fill_with_sno, error_line_out):
     ft_map = {}
     #有效输入字段数（去除默认值后的）
     in_count = 0
     used_row = []
+    #step1-3相当于数据预处理，解析传入选项
+
     #step1 处理映射列 不能和第二步合并
     for to_row in to:
         if r"\:" not in to_row and len(to_row.split(":")) == 2:
@@ -71,12 +79,15 @@ def process(inpath, total, to, outpath, insp="\t", outsp="\t", empty_fill="", fi
 
     #step2 处理默认值列
     for to_row in to:
+        #处理默认值列
         if r"\=" not in str(to_row) and len(str(to_row).split("=")) == 2:
             ft_map.update({int(to_row.split("=")[0]): "d"+to_row.split("=")[1]})
             continue
+        #处理列列映射
         elif r"\:" not in to_row and len(to_row.split(":")) == 2:
             ft_map.update({int(to_row.split(":")[0]): to_row.split(":")[1]})
             continue
+        #其他普通列
         else:
             to_index = 0
             for i in range(1, total + 1):
@@ -96,40 +107,49 @@ def process(inpath, total, to, outpath, insp="\t", outsp="\t", empty_fill="", fi
     else:
         outsp = {0: outsp}
 
-    #开始处理
+    #step4 开始处理每一行
     lines = read_file(inpath)
     f = open(outpath, "w")
     result = []
     for line in lines:
+        #多个输入分隔符情况，使用正则切分成列
         if len(insp.split("|")) > 0:
             parts = re.split(insp, line)
+        #否则使用正常字符串切分成列
         else:
             parts = line.split(insp)
 
+        #正常的，切分后字段数大于等于配置的选项个数
         if len(parts) >= in_count:
             outline = one_line_proc(parts, total, ft_map, outsp, empty_fill, fill_with_sno)
             result.append(outline + "\n")
+        #不正常的，列数少于配置
         else:
+            #若配置了-e 输出，否则列数不符的记录过滤
             if error_line_out:
                 result.append(line + "\n")
 
+    #step5 输出结果
     f.writelines(result)
     f.close()
 
 #特殊的处理入口，处理维度为每一行,目前只有时间处理
 def handler_specal_part(part_str):
     #timestamp 时间处理
+    #时间列，默认必须 TS数字=时间
     if part_str.startswith("TS") and "=" in part_str:
         ts_format = {8: "%Y%m%d",
                      10: "%Y-%m-%d",
                      14: "%Y%m%d%H%M%S",
                      19: "%Y-%m-%d %H:%M:%S"}
         to_l = 0
+        #step1 确认输出的格式 TS8 TS10 TS14 TS19
         if part_str[2] != "=":
             to_l = int(part_str[2:part_str.index("=")])
 
         part_str = part_str.split("=")[1].strip()
         interval = 0
+        #step2 存在时间+-的情况 确认加减区间
         if "+" in part_str:
             inputdate = part_str.split("+")[0].strip()
             interval = int(part_str.split("+")[1].strip())
@@ -147,13 +167,15 @@ def handler_specal_part(part_str):
                 inputdate = part_str
         else:
             inputdate = part_str.strip()
-
+        #step3 将原始时间转为目标时间
         part_str = get_timestamp(inputdate, ts_format, interval)
+
+        #step4 如果定义了输出格式，转换成目标格式，返回
         if to_l > 0:
             part_str = time.strftime(ts_format.get(to_l), time.localtime(int(part_str)))
     return part_str
 
-#获取时间戳的方法
+#将时间由秒转化为目标格式
 def get_timestamp(inputdate, ts_format, interval=0):
     if "now()" in inputdate:
         inputdate = time.strftime("%Y%m%d%H%M%S") 
@@ -186,6 +208,7 @@ def help_msg():
     print("\t -e                [可选，error, 源文件列切分不一致行/空行/注释等，会被直接输出，正确行按原逻辑处理]")
     sys.exit(0)
 
+#判断某个参数必须被定义
 def must_be_defined(param, map, error_info):
     if param not in map:
        print error_info
@@ -193,6 +216,13 @@ def must_be_defined(param, map, error_info):
 
 #程序入口，读入参数，执行
 def main():
+    #init default value
+    insp = "\t"
+    outsp = "\t"
+    empty_fill = ''
+    fill_with_sno = False
+    error_line_out = False
+    #handle options
     try:
         opts,args = getopt.getopt(sys.argv[1:],"F:P:t:a:i:o:f:hse")
 
@@ -237,24 +267,10 @@ def main():
         print(sys.argv[0]+" file : %s is not exists"%inpath)
         sys.exit(1)
 
-    if 'empty_fill' not in dir():
-        empty_fill = ''
-    if 'fill_with_sno' not in dir():
-        fill_with_sno = False
-    if 'error_line_out' not in dir():
-        error_line_out = False
-
     if 'outpath' not in dir():
         outpath = inpath+".dist"
 
-    if 'insp' in dir() and 'outsp' in dir():
-        process(inpath, total, to, outpath, insp, outsp, empty_fill=empty_fill, fill_with_sno=fill_with_sno, error_line_out=error_line_out)
-    elif 'insp' in dir():
-        process(inpath, total, to, outpath, insp, empty_fill=empty_fill, fill_with_sno=fill_with_sno, error_line_out=error_line_out)
-    elif 'outsp' in dir():
-        process(inpath, total, to, outpath, outsp=outsp, empty_fill=empty_fill, fill_with_sno=fill_with_sno, error_line_out=error_line_out)
-    else:
-        process(inpath, total, to, outpath, empty_fill=empty_fill, fill_with_sno=fill_with_sno, error_line_out=error_line_out)
+    process(inpath, total, to, outpath, insp, outsp, empty_fill, fill_with_sno, error_line_out)
 
 if __name__ =="__main__":
     main()
